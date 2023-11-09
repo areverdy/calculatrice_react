908d025ca89c11c9ec6337cbbd49935019eaaa61
areverdy <alec.rev1234@gmail.com>
2023-11-09 14:08:30 +0100

    calculs okay

--- src/Calculatrice.tsx
+++ src/Calculatrice.tsx
@@ -1,68 +1,77 @@
-import { useCallback, useState } from "react"
+import React, { useState } from "react";
 
 const Calculatrice = () => {
-    const [chiffre, setChiffre] = useState("")
-    const [operateur, setOp] = useState("")
-    
-    const handleClick = useCallback((num: number) => {
-        console.log(num);
-        setChiffre((prevChiffre) => (prevChiffre === "0" ? num.toString() : prevChiffre + num.toString()));
-    }, []);
+  const [chiffre, setChiffre] = useState("");
+  const [chiffrePrecedent, setChiffrePrecedent] = useState("");
+  const [operateur, setOp] = useState("");
 
-    const handleClicksigne = useCallback((signe: string) => {
-        console.log(signe);
-        if (chiffre !== "") {
-            setOp(signe);
-            setChiffre("");
-          }
-        }, [chiffre]);
+  const handleClick = (num) => {
+    setChiffre((prevChiffre) => (prevChiffre === "0" ? num.toString() : prevChiffre + num.toString()));
+  };
 
-        const evaluerExpression = (expr: string): string => {
-            try {
-              return String(parseFloat(eval(expr).toFixed(10)));
-            } catch (error) {
-              return "Erreur";
-            }
-          };
-        
-          const calculerResultat = () => {
-            if (operateur !== "" && chiffre !== "") {
-              const expression = chiffre + operateur + chiffre;
-              const resultat = evaluerExpression(expression);
-              setChiffre(resultat);
-              setOp("");
-            }
-          };
+  const handleClickSigne = (signe) => {
+    if (chiffre !== "") {
+      setOp(signe);
+      setChiffrePrecedent(chiffre);
+      setChiffre("");
+    }
+  };
 
-    return (
-        <div>
-            <div className="bloc">
-            <button onClick={() => handleClick(1)}>1</button>
-            <button onClick={() => handleClick(2)}>2</button>
-            <button onClick={() => handleClick(3)}>3</button>
-            <button onClick={() => handleClicksigne("-")}>-</button>
-            </div>
-            <div className="bloc">
-            <button onClick={() => handleClick(4)}>4</button>
-            <button onClick={() => handleClick(5)}>5</button>
-            <button onClick={() => handleClick(6)}>6</button>
-            <button onClick={() => handleClicksigne("+")}>+</button>
-            </div>
-            <div className="bloc">
-            <button onClick={() => handleClick(7)}>7</button>
-            <button onClick={() => handleClick(8)}>8</button>
-            <button onClick={() => handleClick(9)}>9</button>
-            <button onClick={()=> handleClicksigne("C")}>C</button>
-            </div>
-            <div className="bloc">
-            <button onClick={() => handleClick(0)}>0</button>            
-            </div>
-            <button onClick={calculerResultat}>=</button>
-            <div>chiffre : {chiffre === "0" ? "" : chiffre}</div>
-            <div>operateur : {operateur}</div>
-        </div>
+  const evaluerExpression = (num1, num2, operateur) => {
+    switch (operateur) {
+      case "+":
+        return num1 + num2;
+      case "-":
+        return num1 - num2;
+      default:
+        return NaN;
+    }
+  };
 
-    )
-}
+  const calculerResultat = () => {
+    if (operateur !== "" && chiffre !== "") {
+      const num1 = parseFloat(chiffrePrecedent);
+      const num2 = parseFloat(chiffre);
+      if (!isNaN(num1) && !isNaN(num2)) {
+        const resultat = evaluerExpression(num1, num2, operateur);
+        setChiffre(resultat.toString());
+        setOp("");
+        setChiffrePrecedent("");
+      } else {
+        setChiffre("Erreur");
+        setOp("");
+      }
+    }
+  };
 
-export default Calculatrice
+  return (
+    <div>
+      <div className="bloc">
+        <button onClick={() => handleClick(1)}>1</button>
+        <button onClick={() => handleClick(2)}>2</button>
+        <button onClick={() => handleClick(3)}>3</button>
+        <button onClick={() => handleClickSigne("-")}>-</button>
+      </div>
+      <div className="bloc">
+        <button onClick={() => handleClick(4)}>4</button>
+        <button onClick={() => handleClick(5)}>5</button>
+        <button onClick={() => handleClick(6)}>6</button>
+        <button onClick={() => handleClickSigne("+")}>+</button>
+      </div>
+      <div className="bloc">
+        <button onClick={() => handleClick(7)}>7</button>
+        <button onClick={() => handleClick(8)}>8</button>
+        <button onClick={() => handleClick(9)}>9</button>
+        <button onClick={() => handleClickSigne("C")}>C</button>
+      </div>
+      <div className="bloc">
+        <button onClick={() => handleClick(0)}>0</button>
+      </div>
+      <button onClick={calculerResultat}>=</button>
+      <div>chiffre : {chiffre === "0" ? "" : chiffre}</div>
+      <div>operateur : {operateur}</div>
+    </div>
+  );
+};
+
+export default Calculatrice;
---
+++ src/Qwik.md
@@ -0,0 +1,9 @@
+Source Wikipédia et This dot Labs article de Bruce Wiggleston 3/08/22
+
+Qwik est un cadre de travail (framework) open-source de développement web, conçu pour améliorer les performances de chargement des applications web. 
+Créé par Misko Hevery, l'ingénieur en informatique connu pour avoir développé AngularJS, Qwik a été introduit en 2022.
+Qwik a été conçu pour prioriser la performance, en se concentrant sur la rapidité de chargement des applications et sur l'efficacité de l'interaction utilisateur. Il vise à minimiser le temps entre le moment où un utilisateur visite une page et le moment où il peut commencer à interagir avec elle.
+Ce framework est basé sur une méthode appellée Resumability. Contrairement aux autres framework, il n'est pas basé sur ce qui est nommé l'hydration. C'est-à-dire lorsque le contenu est pré-rendu côté serveur, qui prend donc du temps pour le chargement de la page et qui coûte cher. Avec l'hydration le temps de conversion est plus long, et les sites sont de faits moins bien notés que ceux créés avec Qwik.
+Qwuk optimise sa vitesse en fournissant du HTML pur en chargant progressivement JS uniquement en fonction des besoins.
+Pour se faire Qwik, en plus de se soucier du rendu côté client - comme tous les frameworks - se soucie aussi également du rendu sur un serveur, de la sérialisation des données, de la création et de la prélecture des bundles et de la fourniture de tout ce contenu au client.
+En résumé Qwik offre la possibilité de rendre l'application interactive immédiatement après la livraison HTML.
